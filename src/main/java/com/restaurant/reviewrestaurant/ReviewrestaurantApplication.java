6b3e52b1a670fe6eac0851a3fcbec256a2769507
Limbezz <arthur163ru@gmail.com>
2025-07-21 16:47:39 +0400

    prefinal 0.76

--- src/main/java/com/restaurant/reviewrestaurant/ReviewrestaurantApplication.java
+++ src/main/java/com/restaurant/reviewrestaurant/ReviewrestaurantApplication.java
@@ -2,7 +2,11 @@ package com.restaurant.reviewrestaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+
+
+@EntityScan("com.restaurant.reviewrestaurant.entity")
 @SpringBootApplication
 public class ReviewrestaurantApplication {
 
--- src/main/java/com/restaurant/reviewrestaurant/mapper/RestaurantMapper.java
+++ src/main/java/com/restaurant/reviewrestaurant/mapper/RestaurantMapper.java
@@ -5,14 +5,14 @@ import com.restaurant.reviewrestaurant.dto.RestaurantResponseDTO;
 import com.restaurant.reviewrestaurant.entity.Restaurant;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
+import java.math.BigDecimal;
 @Mapper(componentModel = "spring")
 public interface RestaurantMapper {
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "rating", expression = "java(BigDecimal.ZERO)")
-    @Mapping(source = "description", target = "description") // Явный маппинг description
+    @Mapping(target = "rating", expression = "java(java.math.BigDecimal.ZERO)")
+    @Mapping(source = "description", target = "description")
     Restaurant toEntity(RestaurantRequestDTO dto);
 
-    @Mapping(source = "description", target = "description") // Маппинг для response
+    @Mapping(source = "description", target = "description")
     RestaurantResponseDTO toResponseDTO(Restaurant entity);
 }
